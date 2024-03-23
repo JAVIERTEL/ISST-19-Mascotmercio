d0188f71ac0345dc4005220e6f5887ad25691cfa
JAVIERTEL <javierlaguna2001@gmail.com>
2024-03-23 12:38:55 +0100

    first commit

---
+++ src/main/java/ISST/Mascotmercio/MascotmercioApplication.java
@@ -0,0 +1,13 @@
+package ISST.Mascotmercio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascotmercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascotmercioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ISST/Mascotmercio/MascotmercioApplicationTests.java
@@ -0,0 +1,13 @@
+package ISST.Mascotmercio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MascotmercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
