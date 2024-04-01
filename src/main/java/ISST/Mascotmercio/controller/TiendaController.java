f981524510ac0c17aaf518dfecf56d7dc1d85035
mlozan <marialozanocasero@gmail.com>
2024-04-01 18:32:16 +0200

    quitar inicio de sesion y completar tienda controller

--- src/main/java/ISST/Mascotmercio/controller/TiendaController.java
+++ src/main/java/ISST/Mascotmercio/controller/TiendaController.java
@@ -2,7 +2,13 @@ package ISST.Mascotmercio.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +17,7 @@ import ISST.Mascotmercio.service.TiendaService;
 
 @RestController
 @RequestMapping("api/tienda")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TiendaController {
 
     @Autowired
@@ -19,5 +26,22 @@ public class TiendaController {
     @GetMapping("/findAll")
     public List<Tienda> findAll(){
         return tiendaService.findAll();
+    }
+
+    @PostMapping
+    public Tienda createTienda(@RequestBody Tienda tienda) {
+        return tiendaService.createTienda(tienda);
+    }
+
+    @PutMapping("/{id}")
+    public Tienda updateTienda(@PathVariable Long id, @RequestBody Tienda tienda) {
+        // Verifica si el servicio con el ID proporcionado existe antes de actualizarlo
+        tienda.setIdTienda(id);
+        return tiendaService.updateTienda(tienda);
+    }
+    
+    @DeleteMapping("/{id}")
+    public void deleteTienda(@PathVariable Long id) {
+        tiendaService.deleteTienda(id);
     }
 }
