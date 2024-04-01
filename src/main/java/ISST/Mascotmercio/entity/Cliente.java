6d5cc8b21509ae01bc35e9239cb0e6e6c5ae78cc
mlozan <marialozanocasero@gmail.com>
2024-04-01 18:41:42 +0200

    borrar imports innecesarios

--- src/main/java/ISST/Mascotmercio/entity/Cliente.java
+++ src/main/java/ISST/Mascotmercio/entity/Cliente.java
@@ -1,10 +1,7 @@
 package ISST.Mascotmercio.entity;
 
-import java.util.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
--- src/main/java/ISST/Mascotmercio/entity/Tienda.java
+++ src/main/java/ISST/Mascotmercio/entity/Tienda.java
@@ -1,7 +1,5 @@
 package ISST.Mascotmercio.entity;
 
-import java.util.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
